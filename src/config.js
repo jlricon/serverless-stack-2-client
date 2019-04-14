524ba7ba74722ba6920f166aed824f55d33170f8
Jose Luis Ricon <jose@ricon.xyz>
2019-04-14 21:24:44 +0100

    add stripe

--- src/config.js
+++ src/config.js
@@ -38,5 +38,6 @@ const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_XnsWM9V8AtFVP8gXu4DtxuBc007V6MarFV",
   ...config
 };
